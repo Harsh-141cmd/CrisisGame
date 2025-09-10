1ce159b0c656a127ddba5bb95fcb653bebf7d0a4
Harsh-141cmd <harsuk83@gmail.com>
2025-09-11 00:54:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Harsh-141cmd/CrisisGame.git', // Replace with your GitHub repository name
+  base: '/CrisisGame/', // Replace with your actual GitHub repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
